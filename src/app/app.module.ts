452680b597573165f5c94215c8d6ac3058c31c55
eatPorkAndSeePigRun <811302964@qq.com>
2018-01-21 22:22:28 +0800

    HTTP服务

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,37 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
-import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import {HeroesComponent} from './heroes.component';
-import {HeroService} from './hero.service';
-import {RouterModule} from '@angular/router';
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent }         from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {AppRoutingModule} from './app-routing/app-routing.module';
-
+import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService }          from './hero.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroDetailComponent,
-    HeroesComponent,
-    DashboardComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
-  providers: [HeroService],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent
+  ],
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
